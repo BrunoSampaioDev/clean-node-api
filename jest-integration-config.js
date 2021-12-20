91a198a01d1d4d6e289f74e0e2e84eb037ed5d1b
BrunoSampaioDev <sampaionascimento100@gmail.com>
2021-12-20 19:26:14 -0300

    chore: add jest configs and scripts

---
+++ jest-integration-config.js
@@ -0,0 +1,3 @@
+const config = require('./jest.config')
+config.testMacth = ['**/*.test.ts']
+module.exports = config
---
+++ jest-unit-config.js
@@ -0,0 +1,3 @@
+const config = require('./jest.config')
+config.testMacth = ['**/*.spec.ts']
+module.exports = config
