9bad3017161d71ac47457135b8ff802b00f6ece4
BrunoSampaioDev <sampaionascimento100@gmail.com>
2021-12-22 23:11:52 -0300

    refactor: change order instances

--- src/main/factories/signup.ts
+++ src/main/factories/signup.ts
@@ -7,8 +7,8 @@ import { EmailValidatorAdapter } from '../../utils/email-validator-adapter'
 export const makeSignUpController = (): SignUpController => {
   const salt = 12
   const emailValidatorAdapter = new EmailValidatorAdapter()
-  const accountMongoRepository = new AccountMongoRepository()
   const bcryptAdapter = new BcryptAdapter(salt)
+  const accountMongoRepository = new AccountMongoRepository()
   const dbAddAccount = new DbAddAccount(bcryptAdapter, accountMongoRepository)
   return new SignUpController(emailValidatorAdapter, dbAddAccount)
 }
