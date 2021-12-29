afd36411aac110b367018e296b34f44dbd896044
BrunoSampaioDev <sampaionascimento100@gmail.com>
2021-12-29 15:08:41 -0300

    refactor: make server error return the error stack

--- src/main/decorators/log.ts
+++ src/main/decorators/log.ts
@@ -9,6 +9,7 @@ export class LogControllerDecorator implements Controller {
 
   async handle (httpRequest: HttpRequest): Promise<HttpResponse> {
     const httpResponse = await this.controller.handle(httpRequest)
+
     return httpResponse
   }
 }
--- src/presentation/controllers/signup/signup.ts
+++ src/presentation/controllers/signup/signup.ts
@@ -38,7 +38,7 @@ export class SignUpController implements Controller {
       })
       return ok(account)
     } catch (error) {
-      return serverError()
+      return serverError(error)
     }
   }
 }
