2e61c84d7d74e002944cb7aa5c87de35622055d8
BrunoSampaioDev <sampaionascimento100@gmail.com>
2022-01-13 20:53:22 -0300

    refactor: move password comparation to the ValidationComposite

--- src/main/factories/signup-validation.spec.ts
+++ src/main/factories/signup-validation.spec.ts
@@ -1,3 +1,4 @@
+import { CompareFieldsValidation } from '../../presentation/helpers/validators/compare-fields-validation'
 import { RequiredFieldValidation } from '../../presentation/helpers/validators/required-filed-validation'
 import { Validation } from '../../presentation/helpers/validators/validation'
 import { ValidationComposite } from '../../presentation/helpers/validators/validation-composite'
@@ -11,6 +12,7 @@ describe('makeSignUpValidation factory', () => {
 
     for (const field of ['name', 'email', 'password', 'passwordConfirmation']) {
       validations.push(new RequiredFieldValidation(field))
+      validations.push(new CompareFieldsValidation('password', 'passwordConfirmation'))
     }
 
     expect(ValidationComposite).toHaveBeenCalledWith(validations)
--- src/main/factories/signup-validation.ts
+++ src/main/factories/signup-validation.ts
@@ -1,3 +1,4 @@
+import { CompareFieldsValidation } from '../../presentation/helpers/validators/compare-fields-validation'
 import { RequiredFieldValidation } from '../../presentation/helpers/validators/required-filed-validation'
 import { Validation } from '../../presentation/helpers/validators/validation'
 import { ValidationComposite } from '../../presentation/helpers/validators/validation-composite'
@@ -7,6 +8,7 @@ export const makeSignUpValidation = (): ValidationComposite => {
 
   for (const field of ['name', 'email', 'password', 'passwordConfirmation']) {
     validations.push(new RequiredFieldValidation(field))
+    validations.push(new CompareFieldsValidation('password', 'passwordConfirmation'))
   }
 
   return new ValidationComposite(validations)
