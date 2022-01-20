296a46dec6482beb2aaffd3fd70df5dbc0d846d0
BrunoSampaioDev <sampaionascimento100@gmail.com>
2022-01-19 21:39:44 -0300

    refactor: rename test title

--- src/presentation/helpers/validators/required-filed-validation.spec.ts
+++ src/presentation/helpers/validators/required-filed-validation.spec.ts
@@ -5,13 +5,13 @@ const makeSut = (): RequiredFieldValidation => {
   return new RequiredFieldValidation('field')
 }
 describe('RequiredFieldValidation', () => {
-  test('should not return  if validation succeeds', () => {
+  test('should return a MissingParamError if validation fails', () => {
     const sut = makeSut()
     const error = sut.validate({ name: 'any_name' })
     expect(error).toEqual(new MissingParamError('field'))
   })
 
-  test('should return a MissingParamError if validation fails', () => {
+  test('should not return if validation succeeds', () => {
     const sut = makeSut()
     const error = sut.validate({ field: 'any_name' })
     expect(error).toBeFalsy()
