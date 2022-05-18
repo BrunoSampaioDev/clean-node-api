8a358f3a61d5117582ea25910ed8ca8c42f699b9
BrunoSampaioDev <sampaionascimento100@gmail.com>
2022-05-17 22:08:37 -0300

    refactor(validations): move sut creation to a factory method

--- src/presentation/helpers/validators/validation-composite.spec.ts
+++ src/presentation/helpers/validators/validation-composite.spec.ts
@@ -2,15 +2,30 @@ import { MissingParamError } from '../../errors'
 import { Validation } from './validation'
 import { ValidationComposite } from './validation-composite'
 
-describe('ValidationComposite', () => {
+const makeValidation = (): Validation => {
   class ValidationStub implements Validation {
     validate(input: any): Error {
-      return new MissingParamError('filed')
+      return null
     };
   }
+  return new ValidationStub()
+}
+
+interface SutTypes {
+  sut: ValidationComposite
+  validationStub: Validation
+}
+
+const makeSut = (): SutTypes => {
+  const validationStub = makeValidation()
+  const sut = new ValidationComposite([validationStub])
+
+  return { sut, validationStub }
+}
+describe('ValidationComposite', () => {
   test('should return an error if any validation fails', () => {
-    const validationStub = new ValidationStub()
-    const sut = new ValidationComposite([validationStub])
+    const { sut, validationStub } = makeSut()
+    jest.spyOn(validationStub, 'validate').mockReturnValueOnce(new MissingParamError('filed'))
     const error = sut.validate({ filed: 'any_value' })
     expect(error).toEqual(new MissingParamError('filed'))
   })
