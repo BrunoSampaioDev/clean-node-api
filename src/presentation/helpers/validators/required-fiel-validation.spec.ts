1c00c11a03bacf9d428cb7741f01f1ffc7184ff8
BrunoSampaioDev <sampaionascimento100@gmail.com>
2022-05-17 21:35:53 -0300

    refactor(validations): move sut creation to a facotry method

--- src/presentation/helpers/validators/required-fiel-validation.spec.ts
+++ src/presentation/helpers/validators/required-fiel-validation.spec.ts
@@ -1,15 +1,18 @@
 import { MissingParamError } from '../../errors'
 import { RequiredFieldValidation } from './required-fiel-validation'
 
+const makeSut = (): RequiredFieldValidation => {
+  return new RequiredFieldValidation('any_field')
+}
 describe('RequiredFieldValidation', () => {
   test('should return a  MissingParamError if validation fails', () => {
-    const sut = new RequiredFieldValidation('any_field')
+    const sut = makeSut()
     const error = sut.validate({ email: 'any_email@mail.com' })
     expect(error).toEqual(new MissingParamError('any_field'))
   })
 
   test('should not return if validation succeeds', () => {
-    const sut = new RequiredFieldValidation('any_field')
+    const sut = makeSut()
     const error = sut.validate({ any_field: 'any_email@mail.com' })
     expect(error).toBeFalsy()
   })
