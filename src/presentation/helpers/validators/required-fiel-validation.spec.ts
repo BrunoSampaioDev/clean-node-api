5de7541c69c776a3533116add25034b37e5d765c
BrunoSampaioDev <sampaionascimento100@gmail.com>
2022-05-17 21:33:07 -0300

    test(validations): ensure RequiredField not returns a if validation succeeds

--- src/presentation/helpers/validators/required-fiel-validation.spec.ts
+++ src/presentation/helpers/validators/required-fiel-validation.spec.ts
@@ -7,4 +7,10 @@ describe('RequiredFieldValidation', () => {
     const error = sut.validate({ email: 'any_email@mail.com' })
     expect(error).toEqual(new MissingParamError('any_field'))
   })
+
+  test('should not return if validation succeeds', () => {
+    const sut = new RequiredFieldValidation('any_field')
+    const error = sut.validate({ any_field: 'any_email@mail.com' })
+    expect(error).toBeFalsy()
+  })
 })
