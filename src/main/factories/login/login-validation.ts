95ae12816c9ba28517b6d6c85385ebd4db1188ac
BrunoSampaioDev <sampaionascimento100@gmail.com>
2022-05-18 21:13:12 -0300

    refactor(login): move validations LoginController to a ValidationComposite

---
+++ src/main/factories/login/login-validation.ts
@@ -0,0 +1,17 @@
+
+import { EmailValidation } from '../../../presentation/helpers/validators/email-validation'
+import { RequiredFieldValidation } from '../../../presentation/helpers/validators/required-fiel-validation'
+import { Validation } from '../../../presentation/helpers/validators/validation'
+import { ValidationComposite } from '../../../presentation/helpers/validators/validation-composite'
+import { EmailValidatorAdapter } from '../../../utils/email-validator-adapter'
+
+export const makeLoginValidation = (): ValidationComposite => {
+  const validations: Validation[] = []
+  for (const field of ['email', 'password']) {
+    validations.push(new RequiredFieldValidation(field))
+  }
+  const emailValidatorAdapter = new EmailValidatorAdapter()
+  validations.push(new EmailValidation('email', emailValidatorAdapter))
+
+  return new ValidationComposite(validations)
+}
