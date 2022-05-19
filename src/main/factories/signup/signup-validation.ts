a02b5d305c3dbbe20185e38e484e12f5b1348819
BrunoSampaioDev <sampaionascimento100@gmail.com>
2022-05-18 21:12:01 -0300

    refactor(login): move signup factory to a folder

---
+++ src/main/factories/signup/signup-validation.ts
@@ -0,0 +1,20 @@
+
+import { CompareFieldsValidation } from '../../../presentation/helpers/validators/compare-fields-validation'
+import { EmailValidation } from '../../../presentation/helpers/validators/email-validation'
+import { RequiredFieldValidation } from '../../../presentation/helpers/validators/required-fiel-validation'
+import { Validation } from '../../../presentation/helpers/validators/validation'
+import { ValidationComposite } from '../../../presentation/helpers/validators/validation-composite'
+import { EmailValidatorAdapter } from '../../../utils/email-validator-adapter'
+
+export const makeSignUpValidation = (): ValidationComposite => {
+  const validations: Validation[] = []
+  for (const field of ['name', 'email', 'password', 'passwordConfirmation']) {
+    validations.push(new RequiredFieldValidation(field))
+  }
+  validations.push(new CompareFieldsValidation('password', 'passwordConfirmation'))
+
+  const emailValidatorAdapter = new EmailValidatorAdapter()
+  validations.push(new EmailValidation('email', emailValidatorAdapter))
+
+  return new ValidationComposite(validations)
+}
