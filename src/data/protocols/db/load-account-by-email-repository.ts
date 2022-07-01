e8ddf7bb108b10dfa446e7737601093f8a24105c
BrunoSampaioDev <sampaionascimento100@gmail.com>
2022-07-01 20:59:02 -0300

    fix(authentication): correct path to AccountModel in LoadAccountByEmailRepository

--- src/data/protocols/db/load-account-by-email-repository.ts
+++ src/data/protocols/db/load-account-by-email-repository.ts
@@ -1,5 +1,5 @@
-import { AccountModel } from "../../domain/models/account";
+import { AccountModel } from '../../../domain/models/account'
 
 export interface LoadAccountByEmailRepository {
-  load(email: string): Promise<AccountModel>
+  load: (email: string) => Promise<AccountModel>
 }
