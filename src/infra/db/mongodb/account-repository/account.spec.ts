6daae2a48f4dd645e871e8d12ae82446db58b984
BrunoSampaioDev <sampaionascimento100@gmail.com>
2022-07-29 12:29:29 -0300

    test(auth): ensure AccountMongoRepository updates the accessToken on  updateAccesToken success

--- src/infra/db/mongodb/account-repository/account.spec.ts
+++ src/infra/db/mongodb/account-repository/account.spec.ts
@@ -1,4 +1,4 @@
-import { Collection } from 'mongodb'
+import { Collection, ObjectId } from 'mongodb'
 import { MongoHelper } from '../helpers/mongo-helper'
 import { AccountMongoRepository } from './account'
 
@@ -55,5 +55,18 @@ describe('Account mongo Repository', () => {
 
     const account = await sut.loadByEmail('any_email@mail.com')
     expect(account).toBeFalsy()
+  })
+
+  test('Should update the account accessToken on updateAccessToken success', async () => {
+    const sut = makeSut()
+    const fakeAccount = await accountCollection.insertOne({
+      name: 'any_name',
+      email: 'any_email@mail.com',
+      password: 'any_password'
+    })
+    await sut.updateAccessToken(fakeAccount.insertedId.toString(), 'any_token')
+    const account = await accountCollection.findOne({ _id: new ObjectId(fakeAccount.insertedId.toString()) })
+    expect(account).toBeTruthy()
+    expect(account?.accessToken).toBe('any_token')
   })
 })
